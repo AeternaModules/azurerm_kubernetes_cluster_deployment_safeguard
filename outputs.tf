62f85332337987780ffbe050818a94ecaccc1531
Daniel Allen <dan@aeternagroup.net>
2026-07-16 14:48:35 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,21 @@
 output "kubernetes_cluster_deployment_safeguards_id" {
   description = "Map of id values across all kubernetes_cluster_deployment_safeguards, keyed the same as var.kubernetes_cluster_deployment_safeguards"
-  value       = { for k, v in azurerm_kubernetes_cluster_deployment_safeguard.kubernetes_cluster_deployment_safeguards : k => v.id }
+  value       = { for k, v in azurerm_kubernetes_cluster_deployment_safeguard.kubernetes_cluster_deployment_safeguards : k => v.id if v.id != null && length(v.id) > 0 }
 }
 output "kubernetes_cluster_deployment_safeguards_excluded_namespaces" {
   description = "Map of excluded_namespaces values across all kubernetes_cluster_deployment_safeguards, keyed the same as var.kubernetes_cluster_deployment_safeguards"
-  value       = { for k, v in azurerm_kubernetes_cluster_deployment_safeguard.kubernetes_cluster_deployment_safeguards : k => v.excluded_namespaces }
+  value       = { for k, v in azurerm_kubernetes_cluster_deployment_safeguard.kubernetes_cluster_deployment_safeguards : k => v.excluded_namespaces if v.excluded_namespaces != null && length(v.excluded_namespaces) > 0 }
 }
 output "kubernetes_cluster_deployment_safeguards_kubernetes_cluster_id" {
   description = "Map of kubernetes_cluster_id values across all kubernetes_cluster_deployment_safeguards, keyed the same as var.kubernetes_cluster_deployment_safeguards"
-  value       = { for k, v in azurerm_kubernetes_cluster_deployment_safeguard.kubernetes_cluster_deployment_safeguards : k => v.kubernetes_cluster_id }
+  value       = { for k, v in azurerm_kubernetes_cluster_deployment_safeguard.kubernetes_cluster_deployment_safeguards : k => v.kubernetes_cluster_id if v.kubernetes_cluster_id != null && length(v.kubernetes_cluster_id) > 0 }
 }
 output "kubernetes_cluster_deployment_safeguards_level" {
   description = "Map of level values across all kubernetes_cluster_deployment_safeguards, keyed the same as var.kubernetes_cluster_deployment_safeguards"
-  value       = { for k, v in azurerm_kubernetes_cluster_deployment_safeguard.kubernetes_cluster_deployment_safeguards : k => v.level }
+  value       = { for k, v in azurerm_kubernetes_cluster_deployment_safeguard.kubernetes_cluster_deployment_safeguards : k => v.level if v.level != null && length(v.level) > 0 }
 }
 output "kubernetes_cluster_deployment_safeguards_pod_security_standards_level" {
   description = "Map of pod_security_standards_level values across all kubernetes_cluster_deployment_safeguards, keyed the same as var.kubernetes_cluster_deployment_safeguards"
-  value       = { for k, v in azurerm_kubernetes_cluster_deployment_safeguard.kubernetes_cluster_deployment_safeguards : k => v.pod_security_standards_level }
+  value       = { for k, v in azurerm_kubernetes_cluster_deployment_safeguard.kubernetes_cluster_deployment_safeguards : k => v.pod_security_standards_level if v.pod_security_standards_level != null && length(v.pod_security_standards_level) > 0 }
 }
 
